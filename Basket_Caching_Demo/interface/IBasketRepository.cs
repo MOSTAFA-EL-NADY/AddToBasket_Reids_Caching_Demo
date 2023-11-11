efca1850f549bf0f3c8409caad10fac3b9f671f6
mostafaelnady1997 <mostafaelnady1997@gmail.com>
2023-11-12 01:00:50 +0200

    Add project files.

---
+++ Basket_Caching_Demo/interface/IBasketRepository.cs
@@ -0,0 +1,10 @@
+﻿using Basket_Caching_Demo.Models;
+
+namespace Basket_Caching_Demo.Interface;
+public interface IBasketRepository
+{
+    Task<UserBasket> GetBasketAsync(string basketId);
+    Task<UserBasket> UpdateOrCreateBasketAsync(UserBasket basket);
+    Task<bool> DeleteBasketAsync(string basketId);
+}
+
